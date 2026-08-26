1978404f83bfa11bbd4c8d4a1f59ae904cbd15ba
BarreroDev <barrerodev@gmail.com>
2026-08-26 05:51:01 +0200

    Implementación de capa servicio dueño terminada version 1.0

--- back/src/main/java/com/veterinaria/back/service/DuenosServiceImpl.java
+++ back/src/main/java/com/veterinaria/back/service/DuenosServiceImpl.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import com.veterinaria.back.model.Dueno;
+import com.veterinaria.back.repository.DuenoRepository;
+
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Implementación de la lógica de negocio para los dueños.
@@ -17,40 +20,88 @@ import com.veterinaria.back.model.Dueno;
 @Service
 public class DuenosServiceImpl implements DuenosService {
 
+	private final DuenoRepository duenoRepository;
+	
+	public DuenosServiceImpl(DuenoRepository duenoRepository) {
+		this.duenoRepository = duenoRepository;
+	}
+	
+	/**
+	 * Método para listar todos los dueños.
+	 */
 	@Override
+	@Transactional(readOnly = true)
 	public List<Dueno> obtenerTodos() {
 		// TODO Auto-generated method stub
-		return null;
+		return duenoRepository.findAll();
 	}
 
+	/**
+	 * Método para filtrar por Id.
+	 * Asegurando solo la lectura.
+	 */
 	@Override
+	@Transactional(readOnly = true)
 	public Optional<Dueno> obtenerPorId(Long id) {
 		// TODO Auto-generated method stub
-		return Optional.empty();
+		return duenoRepository.findById(id);
 	}
 
+	/**
+	 * Método para filtrar por Dni.
+	 * Aseguramos solo la lectura.
+	 */
 	@Override
+	@Transactional(readOnly = true)
 	public Optional<Dueno> obtenerPorDni(String dni) {
 		// TODO Auto-generated method stub
-		return Optional.empty();
+		return duenoRepository.findByDni(dni);
 	}
 
+	/**
+	 * Método para guardar un nuevo dueño.
+	 * si no existe lo inserta y en caso contrario frena y no genera datos duplicados o corruptos.
+	 */
 	@Override
-	public Dueno guardar(Dueno dueno) {
-		// TODO Auto-generated method stub
-		return null;
+	@Transactional
+	public Dueno guardar(Dueno dueno)  {
+		if(duenoRepository.existsByDni(dueno.getDni())) {
+		   throw new IllegalArgumentException("Ya existe un dueño registrado con el DNI: " + dueno.getDni());	
+		}
+		return duenoRepository.save(dueno);
 	}
 
+	/**
+	 * Método para modificar datos de un dueño.
+	 * En el caso de no existir nos avisa.
+	 */
 	@Override
+	@Transactional
 	public Dueno actualizar(Long id, Dueno duenoActualizado) {
-		// TODO Auto-generated method stub
-		return null;
+	    return duenoRepository.findById(id)
+	            .map(duenoExistente -> {
+	                duenoExistente.setNombre(duenoActualizado.getNombre());
+	                duenoExistente.setApellidos(duenoActualizado.getApellidos());
+	                duenoExistente.setTelefono(duenoActualizado.getTelefono());
+	                duenoExistente.setEmail(duenoActualizado.getEmail());
+	                duenoExistente.setFechaNacimiento(duenoActualizado.getFechaNacimiento());
+	                duenoExistente.setFotoUrl(duenoActualizado.getFotoUrl());
+	                return duenoRepository.save(duenoExistente);
+	            })
+	            .orElseThrow(() -> new RuntimeException("Dueño no encontrado con ID: " + id));
 	}
 
+	/**
+	 * Método para eliminar usuario.
+	 * 
+	 */
 	@Override
+	@Transactional
 	public void eliminiarporId(Long id) {
-		// TODO Auto-generated method stub
-
+		if(!duenoRepository.existsById(id)) {
+			throw new RuntimeException("No se puede eliminar. Dueño no encontrado con ID: " + id);
+        }
+		duenoRepository.deleteById(id);
 	}
 
 }
