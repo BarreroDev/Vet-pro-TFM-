57e89fd8f8f8e287496d3ba9ee2230ea71900e49
BarreroDev <barrerodev@gmail.com>
2026-08-31 12:41:03 +0200

    controlador dueño empezado

--- back/src/main/java/com/veterinaria/back/controller/DuenoController.java
+++ back/src/main/java/com/veterinaria/back/controller/DuenoController.java
@@ -1,5 +1,27 @@
 package com.veterinaria.back.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.veterinaria.back.service.DuenosService;
+
+/**
+ * Controlador REST para la gestión de Dueños de mascotas.
+ * 
+ * @author Roberto Barrero de la Rosa
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("/api/duenos")
+@CrossOrigin(origins = "http://localhost:4200")
 public class DuenoController {
+	
+	private final DuenosService duenosService;
+	
+	public DuenoController(DuenosService duenosService) {
+		this.duenosService = duenosService;
+	}
+	
 
 }
