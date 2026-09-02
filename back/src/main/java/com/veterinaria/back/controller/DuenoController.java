365258da406fd6dea06dc439e1d7102a8e77bde2
BarreroDev <barrerodev@gmail.com>
2026-09-02 17:17:12 +0200

    Métodos post , put, delete en entidad dueño terminadas

--- back/src/main/java/com/veterinaria/back/controller/DuenoController.java
+++ back/src/main/java/com/veterinaria/back/controller/DuenoController.java
@@ -2,12 +2,19 @@ package com.veterinaria.back.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.PutExchange;
 
 import com.veterinaria.back.model.Dueno;
 import com.veterinaria.back.service.DuenosService;
@@ -29,11 +36,20 @@ public class DuenoController {
 		this.duenosService = duenosService;
 	}
 	
+	/**
+	 * Método que trae todos los dueños.
+	 * @return la liitar con todos los dueños.
+	 */
 	@GetMapping
 	public ResponseEntity<List<Dueno>> obtenerTodos() {
 		return ResponseEntity.ok(duenosService.obtenerTodos());
 	}
 	
+	/**
+	 * Método para filtrar.
+	 * @param id
+	 * @return un dueño por su Id.
+	 */
 	@GetMapping("/{id}")
 	public ResponseEntity<Dueno> obternerPorId(@PathVariable Long id){
 		return duenosService.obtenerPorId(id)
@@ -41,11 +57,52 @@ public class DuenoController {
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
+	/**
+	 * Método para filtrar.
+	 * @param dni
+	 * @return un dueño por su dni.
+	 */
 	@GetMapping("/buscar/dni/{dni}")
 	public ResponseEntity<Dueno> obternerPorDni(@PathVariable String dni){
 		return duenosService.obtenerPorDni(dni)
 				.map(ResponseEntity::ok)
 				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	/**
+	 * Método para crear un dueño.
+	 * @param dueno
+	 * @return un nuevo dueño
+	 */
+	@PostMapping
+	public ResponseEntity<Dueno> guardar(@RequestBody Dueno dueno){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(duenosService.guardar(dueno));	
+	}
+	
+	/**
+	 * Método para modificar.
+	 * @param id
+	 * @param dueno
+	 * @return datos modificados de un dueño.
+	 */
+	@PutMapping("/{id}")
+	public ResponseEntity<Dueno> actualizar(@PathVariable Long id, @RequestBody Dueno dueno){
+		try {
+			return ResponseEntity.ok(duenosService.actualizar(id, dueno));
+		} catch (RuntimeException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Dueno> eliminar(@PathVariable Long id){
+		try {
+			duenosService.eliminiarporId(id);
+			return ResponseEntity.noContent().build();
+		} catch (RuntimeException e) {
+			return ResponseEntity.notFound().build();
+		}
 	}
 	
 
