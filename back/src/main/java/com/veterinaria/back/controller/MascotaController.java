1a298bd69f015f0fe173d6b45197fcd99891d557
BarreroDev <barrerodev@gmail.com>
2026-09-02 18:13:07 +0200

    Métodos Get en mascotasContoller terminado

--- back/src/main/java/com/veterinaria/back/controller/MascotaController.java
+++ back/src/main/java/com/veterinaria/back/controller/MascotaController.java
@@ -1,5 +1,47 @@
 package com.veterinaria.back.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.veterinaria.back.model.Mascota;
+import com.veterinaria.back.service.MascotasService;
+
+/**
+ * Controlador REST para la gestión de Mascotas.
+ * 
+ * @author Roberto Barrero de la Rosa
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("/api/mascotas")
+@CrossOrigin(origins = "http://localhost:4200")
 public class MascotaController {
 
+	private final MascotasService mascotasService;
+	
+	public MascotaController(MascotasService mascotasService){
+		this.mascotasService = mascotasService;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Mascota>> obtenerTodas(){
+		return ResponseEntity.ok(mascotasService.obtenerTodas());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Mascota> obtenerPorId(@PathVariable Long id){
+		return mascotasService.obtenerPorId(id).map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/dueno/dni/{dni}")
+	public ResponseEntity<List<Mascota>> obtenerPorDniDueno(@PathVariable String dni){
+		return ResponseEntity.ok(mascotasService.obtenerPorDniDueno(dni));
+	}
 }
