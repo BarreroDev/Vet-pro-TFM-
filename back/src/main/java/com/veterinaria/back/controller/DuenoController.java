19e08119e207571089a4378de5c86376e89e1901
BarreroDev <barrerodev@gmail.com>
2026-09-02 16:39:35 +0200

    Métodos Get de la entidad Dueño terminadas

--- back/src/main/java/com/veterinaria/back/controller/DuenoController.java
+++ back/src/main/java/com/veterinaria/back/controller/DuenoController.java
@@ -1,9 +1,15 @@
 package com.veterinaria.back.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.veterinaria.back.model.Dueno;
 import com.veterinaria.back.service.DuenosService;
 
 /**
@@ -21,6 +27,25 @@ public class DuenoController {
 	
 	public DuenoController(DuenosService duenosService) {
 		this.duenosService = duenosService;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Dueno>> obtenerTodos() {
+		return ResponseEntity.ok(duenosService.obtenerTodos());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Dueno> obternerPorId(@PathVariable Long id){
+		return duenosService.obtenerPorId(id)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/buscar/dni/{dni}")
+	public ResponseEntity<Dueno> obternerPorDni(@PathVariable String dni){
+		return duenosService.obtenerPorDni(dni)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 	}
 	
 
