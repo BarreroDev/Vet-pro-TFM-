e48136962c599aa6f922823b49bb53963b5c8e44
BarreroDev <barrerodev@gmail.com>
2026-09-06 12:37:09 +0200

    Métodos patch y delete terminados en consultasController

--- back/src/main/java/com/veterinaria/back/controller/ConsultaController.java
+++ back/src/main/java/com/veterinaria/back/controller/ConsultaController.java
@@ -2,12 +2,15 @@ package com.veterinaria.back.controller;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -68,7 +71,28 @@ public class ConsultaController {
 				.body(consultaService.guardar(consulta, mascotaId, veterinarioId));
 	}
 	
+	@PatchMapping("/{id}/estado")
+	public ResponseEntity<Consulta> actualizarEstado(
+	        @PathVariable Long id,
+	        @RequestBody Map<String, String> payload) {
+	    try {
+	        String nuevoEstado = payload.get("estado");
+	        String observaciones = payload.get("observaciones");
+	        return ResponseEntity.ok(consultaService.actualizarEstado(id, nuevoEstado, observaciones));
+	    } catch (RuntimeException e) {
+	        return ResponseEntity.notFound().build();
+	    }
+	}
 	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+	    try {
+	        consultaService.eliminarPorId(id);
+	        return ResponseEntity.noContent().build();
+	    } catch (RuntimeException e) {
+	        return ResponseEntity.notFound().build();
+	    }
+	}
 	
 	
 }
